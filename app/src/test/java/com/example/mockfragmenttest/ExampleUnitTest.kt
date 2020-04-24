3767958befb2fb5b88e417a6467bf14b8d4a0e61
badawiizHell4me! <badawiiz@mail.uc.edu>
2020-04-24 15:50:59 -0400

    unit tests!

--- app/src/test/java/com/example/mockfragmenttest/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mockfragmenttest/ExampleUnitTest.kt
@@ -1,17 +1,33 @@
 package com.example.mockfragmenttest
 
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ApplicationUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun applicationsDTO_maintainsAppInfo() {
+        var app = Applications("facebook", "face@mail.com")
+        assertTrue(app.app.equals("facebook"))
+        assertTrue(app.userName.equals("face@mail.com"))
+    }
+
+    @Test
+    fun applicationsDTO_maintainsAppInfo2() {
+        var app = Applications("twitter", "twitter@mail.com")
+        assertTrue(app.app.equals("twitter"))
+        assertTrue(app.userName.equals("twitter@mail.com"))
     }
 }
